d3cc3580b6a4d5480b71cdaa96e571929a2eebc0
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-02-01 02:20:45 +0200

    .

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,4 +1,38 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const datetimePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    // Отримуємо поточну дату
+    const currentDate = new Date();
+    if (userSelectedDate.getTime() <= currentDate.getTime()) {
+      startBtn.disabled = true;
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future!',
+        position: 'topRight',
+      });
+    } else {
+      startBtn.disabled = false;
+      iziToast.success({
+        title: 'OK!',
+        message: 'You can press Start!',
+        position: 'center',
+      });
+    }
+  },
+};
+flatpickr(datetimePicker, options);
